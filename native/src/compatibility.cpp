a78f737792d9ff5832746eb10d9d59c69cd8040c
MarcinZiabek <marcin@ziabek.com>
2026-06-22 18:28:44 +0200

    Refactor: rename `questpdf_skia_get_questpdf_version` to `questpdf_get_compatibility_version` and mark the old method as obsolete for API consistency

--- native/src/compatibility.cpp
+++ native/src/compatibility.cpp
@@ -3,12 +3,17 @@
 
 extern "C" {
 
-QUEST_API int questpdf_skia_get_questpdf_version() {
+QUEST_API int questpdf_get_compatibility_version() {
     return 15;
 }
 
 QUEST_API int questpdf_skia_check_compatibility_by_calculating_sum(int a, int b) {
     return a + b;
+}
+
+// obsolete
+QUEST_API int questpdf_skia_get_questpdf_version() {
+    return questpdf_get_compatibility_version();
 }
 
 }
