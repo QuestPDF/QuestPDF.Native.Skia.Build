0887ba4a6b4d0d10531fa1b2a8f343f466a5db35
Marcin Ziąbek <marcin@ziabek.com>
2026-07-10 16:56:40 +0200

    Update compatibility.cpp

--- native/src/compatibility.cpp
+++ native/src/compatibility.cpp
@@ -7,4 +7,8 @@ QUEST_API int questpdf_skia_get_compatibility_version() {
     return 17;
 }
 
+QUEST_API int questpdf_get_compatibility_version() {
+    return questpdf_skia_get_compatibility_version();
+}
+
 }
